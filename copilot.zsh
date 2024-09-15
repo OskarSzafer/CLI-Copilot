d3d22d887a42ce6c0fee12a872001c8e21d564c8
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2024-09-15 12:58:53 +0200

    Better context file generation

--- copilot.zsh
+++ copilot.zsh
@@ -6,27 +6,21 @@ OPTIONS_FILE="${SCRIPT_DIR}/.options"
 CONTEXT_FILE="${SCRIPT_DIR}/.context"
 
 _update_context() {
-    local word="$1"
-    local history=$(history | tail -n 10)
-    local ls_output=$(ls)
-    local ls2_output=$(ls ..)
+    # local his=$(history | tail -n 10)
+    local his=$(fc -l)
 
-    # Save the variables to a file instead of calling the Python script
-    {
-        echo "Given the current terminal line and command history, suggest a completion for the current command."
-        echo "Return completed terminal line, without any explanation or formatting."
-        echo "Current terminal line:" 
-        echo "'$word'"
-        echo "Command history:"
-        echo "'"
-        echo "$history"
-        echo "'"
-    } > $CONTEXT_FILE
+    print -r "$BUFFER" > $CONTEXT_FILE
+    pwd >> $CONTEXT_FILE
+    print -r "$his" >> $CONTEXT_FILE
+    ls >> $CONTEXT_FILE
+}
+
+_update_context_buffer() {
+    sed -i "1s/.*/$(printf "%s" "${BUFFER:-}" | sed 's/[\/&]/\\&/g')/" "$CONTEXT_FILE"
 }
 
 _update_postdisplay() {
-    local word="$BUFFER"
-    suggestion=$(grep -i "^$word" "$OPTIONS_FILE" | head -n 1)
+    suggestion=$(grep -i "^$BUFFER" "$OPTIONS_FILE" | head -n 1)
     POSTDISPLAY="${suggestion#$BUFFER}"
     zle redisplay
 }
@@ -39,14 +33,18 @@ TRAPALRM() {
 # Set the timer interval to 1 second
 TMOUT=1
 
-
 # Widget function for autosuggestions
-_autosuggestion_widget() {
-    _update_context "$BUFFER"
+_autosuggestion_widget_newline() {
+    _update_context
     _update_postdisplay
 }
 
-# Widget function for autosuggestions
+_autosuggestion_widget_buffer_change() {
+    _update_context_buffer
+    _update_postdisplay
+}
+
+# TODO: remove this
 insert_autosuggestion() {
     if [[ -n "$suggestion" ]]; then
         # Insert the remaining part of the suggestion into the line
@@ -58,10 +56,10 @@ insert_autosuggestion() {
 
 # Set up hooks to update suggestions as you type
 autoload -U add-zle-hook-widget
-add-zle-hook-widget line-init _autosuggestion_widget
-add-zle-hook-widget keymap-select _autosuggestion_widget
-add-zle-hook-widget zle-line-pre-redraw _autosuggestion_widget
+add-zle-hook-widget line-init _autosuggestion_widget_newline
+add-zle-hook-widget zle-line-pre-redraw _autosuggestion_widget_buffer_change
 
-# Bind the widget to a key (e.g., right arrow)
+# TODO: remove this
+# Bind the widget to a key (tab)
 zle -N insert_autosuggestion_widget insert_autosuggestion
 bindkey '^I' insert_autosuggestion_widget
--- generate_completition.py
+++ generate_completition.py
@@ -1,25 +1,45 @@
 import os
 import time
+import linecache
 
 import google.generativeai as genai
-
 
 OPTIONS_FILE=".options"
 CONTEXT_FILE=".context"
 
-model = None
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+template = """
+You are command line copilot.
+Command line history for context:
+{command_history}
+Output of ls command:
+{ls_output}
+Current working dirrectory: {current_directory}
+Curremt command line prompt: {current_prompt}
+Propose how current prompt should be completed,
+return completed command line prompt aleone with no explanation.
+"""
+
 
 def read_file():
     with open(CONTEXT_FILE, 'r') as file:
-        data = file.read()
+        lines = file.readlines()
     
-    return data
+    current_prompt = lines[0].strip()
+    current_directory = lines[1].strip()
+    command_history = "".join(lines[3:18])
+    ls_output = "".join(lines[18:])
+
+    return template.format(command_history=command_history, ls_output=ls_output, current_directory=current_directory, current_prompt=current_prompt)
 
 def save_output(output):
     with open(OPTIONS_FILE, 'w') as file:
         file.write(output)
 
-def watch_file(check_interval):
+def watch_file(check_interval, min_idle_time):
     # Get the initial modification time of the file
     last_modified_time = os.path.getmtime(CONTEXT_FILE)
 
@@ -27,7 +47,7 @@ def watch_file(check_interval):
         # Check the modification time of the file
         current_modified_time = os.path.getmtime(CONTEXT_FILE)
         
-        if current_modified_time != last_modified_time:
+        if current_modified_time != last_modified_time and (time.time() - current_modified_time) > min_idle_time:
             try:
                 prompt = read_file()
             except Exception as e:
@@ -40,7 +60,7 @@ def watch_file(check_interval):
             try:
                 chat = model.start_chat(history=[])
                 response = chat.send_message(prompt)
-                completion = response.text
+                completion = response.text.strip()
             except Exception as e:
                 print(e)
                 continue
@@ -59,8 +79,4 @@ def watch_file(check_interval):
 
 
 if __name__ == "__main__":
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-    genai.configure(api_key=GOOGLE_API_KEY)
-    model = genai.GenerativeModel('gemini-1.5-flash')
-
-    watch_file(1)
+    watch_file(0.1, 0.3)
