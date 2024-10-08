0e190045a277c34f60a063cc7a1e362715de1e66
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2024-10-08 12:15:38 +0200

    ruff format

--- generate_completion.py
+++ generate_completion.py
@@ -16,7 +16,7 @@ MIN_IDLE_TIME = 0.3
 CHECK_INTERVAL = 0.1
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 prompt_template = """
 You are command line copilot.
@@ -32,37 +32,40 @@ return completed command line prompt aleone with no explanation.
 
 
 def parse_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
-    
+
     current_prompt = lines[0].strip()
     current_directory = lines[1].strip()
     command_history = "".join(lines[3:18])
     ls_output = "".join(lines[18:])
 
     return prompt_template.format(
-        command_history=command_history, 
-        ls_output=ls_output, 
-        current_directory=current_directory, 
-        current_prompt=current_prompt
-        )
+        command_history=command_history,
+        ls_output=ls_output,
+        current_directory=current_directory,
+        current_prompt=current_prompt,
+    )
 
 
 def serve_terminal_session(context_file_path, option_file_path):
     context_modification_time = os.path.getmtime(context_file_path)
     options_modification_time = os.path.getmtime(option_file_path)
 
-    if context_modification_time > options_modification_time and (time.time() - context_modification_time) > MIN_IDLE_TIME:
+    if (
+        context_modification_time > options_modification_time
+        and (time.time() - context_modification_time) > MIN_IDLE_TIME
+    ):
         prompt = parse_file(context_file_path)
 
         if not prompt:
             return
-        
+
         chat = model.start_chat(history=[])
         response = chat.send_message(prompt)
         completion = response.text.strip()
 
-        with open(option_file_path, 'w') as file:
+        with open(option_file_path, "w") as file:
             file.write(completion)
 
 
@@ -70,7 +73,9 @@ def handle_tmp_files(directory):
     for f in tmp_files_dir_path.glob("*.context*"):
         context_file_path = os.path.join(directory, f.name)
 
-        for option_f in tmp_files_dir_path.glob(f.name.replace(".context", ".options")[:-6] + "*"):
+        for option_f in tmp_files_dir_path.glob(
+            f.name.replace(".context", ".options")[:-6] + "*"
+        ):
             option_file_path = os.path.join(directory, option_f)
 
         try:
@@ -82,7 +87,7 @@ def handle_tmp_files(directory):
 def watch_files():
     while True:
         handle_tmp_files(TMP_FILES_DIR)
-        
+
         # Limit frequency of checking for chenges
         time.sleep(CHECK_INTERVAL)
 
