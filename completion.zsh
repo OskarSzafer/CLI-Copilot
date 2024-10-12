272d3a8496e8aaae96e0a2871b2c6b3a472a2e78
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2024-10-12 22:50:02 +0200

    better special characters escape

--- completion.zsh
+++ completion.zsh
@@ -5,7 +5,7 @@ previous_suggestion=""
 
 # Define a custom strategy for zsh-autosuggest
 _zsh_autosuggest_strategy_my_custom_suggestion() {
-    typeset -g suggestion=$(grep -i "^$BUFFER" "$OPTIONS_FILE" | head -n 1)
+    typeset -g suggestion=$(grep -i "^$(_zsh_autosuggest_escape_command "${BUFFER:-}")" "$OPTIONS_FILE" | head -n 1)
 }
 
 _update_postdisplay() {
